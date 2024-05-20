138ae369152a1dd80b951d9e60c51ff43b0be673
Stephen Aranda <aranda.stephen88@gmail.com>
2024-05-19 21:03:04 -0500

    added styling rules

--- aki-retro-game-shop/src/views/RegisterView.vue
+++ aki-retro-game-shop/src/views/RegisterView.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="register">
-    <h1>This is an about page</h1>
+    
   </div>
 </template>
 
 <style scoped>
    .register{
+      background-image: url('../assets/RegisterAkiImg.png');
+      background-attachment: scroll;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      height: 100vh;
+      width:100%;
     
    }
 
