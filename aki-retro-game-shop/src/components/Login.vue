b5a46caf8c9dd68f061a58114d6036e6aa180712
Stephen Aranda <aranda.stephen88@gmail.com>
2024-05-23 19:24:03 -0500

    inserted field set inputs errors and button

--- aki-retro-game-shop/src/components/Login.vue
+++ aki-retro-game-shop/src/components/Login.vue
@@ -4,5 +4,35 @@
 
 <template>
     <SignedOutNav/>
+    <h1>Login</h1>
+    <fieldset>
+        <legend>Login Credentials</legend>
+        <label for="username">Username:</label>
+        <input
+           id="username"
+           type="text"
+           name="username"
+           placeholder="username"
+           required
+
+        />
+
+        <div class="errors">
+
+        </div>
+
+        <label for="password">Password:</label>
+        <input
+           id="password"
+           type="password"
+           name="password"
+           placeholder="Password"
+           required
+        />
+        <div class="errors">
+
+        </div>
+    </fieldset>
+    <button class="register-btn" type="submit">Login</button>
     
 </template>
