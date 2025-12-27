46994e8be6d9d2f5ab01c537639fb59b9cdbff2b
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-26 19:54:55 -0600

    added generic api call template with base api url and path

--- aki-retro-game-shop/src/api/client.js
+++ aki-retro-game-shop/src/api/client.js
@@ -3,3 +3,19 @@
  * File   : client.js
  * Desc   : api client for session cookies
  */
+
+// import env for api backend
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
+
+const apiFetch = async (path, options = {}) => {
+
+    const res = await fetch(`${API_BASE},${path}`, {
+        credentials: "include", // sends/ receives session cookies
+        headers: {
+            "Content-type": "application/json",
+            ...(options.headers || {})
+
+        },
+        ...options
+    })
+}
