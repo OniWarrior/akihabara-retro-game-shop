43117e97c2bcf19c2c94c5651d0dd2a52e333042
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-26 20:30:41 -0600

    created post login api call

--- aki-retro-game-shop/src/stores/AuthStore.js
+++ aki-retro-game-shop/src/stores/AuthStore.js
@@ -24,16 +24,74 @@ export const useAuthStore = defineStore('auth', {
     // action suite - api calls
     actions: {
 
+        // check if user is authenticated
         async checkStatus() {
 
-            // start of api call
+            try {
 
-            // set loading and error to true and null
-            this.loading = true;
-            this.error = null;
-        }
+
+                // start of api call
+
+                // set loading and error to true and null
+                this.loading = true;
+                this.error = null;
+
+                // make api call
+                const response = await apiFetch("/auth/status");
+
+                // assign user session object to this.user if the user is authenticated.
+                this.user = response.authenticated ? response.user : null;
+
+                // checked if the user is authenticated
+                this.checked = true;
+                return response;
+
+            } catch (err) {
+                // If backend uses 401 for "not logged in"
+                if (err.status === 401) {
+                    this.user = null;
+                    this.checked = true;
+                    return;
+                }
+                this.error = err.message;
+                throw err;
+            } finally {
+                this.loading = false;
+            }
+
+        },
+
+        // post login
+        async login({ username, password }) {
+
+
+            try {
+
+                // Start the api call
+                // set loading and error to true and null.
+                this.loading = true;
+                this.error = null;
+
+                // make api call
+                const response = await apiFetch("/auth/login", {
+                    method: "POST",
+                    body: JSON.stringify({ username, password }),
+                });
+
+
+
+                return response;
+            } catch (err) {
+                this.error = err.message;
+                throw err;
+            } finally {
+                this.loading = false;
+            }
+        },
 
     }
+
+
 
 
 })
