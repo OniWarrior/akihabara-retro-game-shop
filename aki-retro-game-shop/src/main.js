4bb1f9d61230a91cfc51e0d3be450e47773f2ba1
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-29 17:40:18 -0600

    added csrf interceptors to retrieve token

--- aki-retro-game-shop/src/main.js
+++ aki-retro-game-shop/src/main.js
@@ -3,12 +3,23 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { attachCsrfInterceptors } from "@/lib/axios";
+import { useCsrfStore } from "@/stores/csrf.store";
+
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia())
-app.use(router)
+app.use(pinia);
+app.use(router);
+
+const csrf = useCsrfStore(pinia);
+
+attachCsrfInterceptors({
+    getToken: () => csrf.token,
+    refreshToken: () => csrf.fetchToken(true),
+});
 
 app.mount('#app')
