9a1edebddb6f0feb249cc34eff404971cbda01a6
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-29 17:13:12 -0600

    added a user store along with imports

---
+++ aki-retro-game-shop/src/stores/UserStore.js
@@ -0,0 +1,8 @@
+/*
+ * Author : Stephen Aranda
+ * File   : UserStore.js
+ * Desc   : Store that is used to track user state.
+ */
+
+import { defineStore } from "pinia";
+import api from "@/api/client";
