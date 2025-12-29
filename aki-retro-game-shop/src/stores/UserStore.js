b231fbba642aafd8ca67fddce4fc048dc800d315
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-29 17:16:52 -0600

    added a user store with state obj

--- aki-retro-game-shop/src/stores/UserStore.js
+++ aki-retro-game-shop/src/stores/UserStore.js
@@ -6,3 +6,11 @@
 
 import { defineStore } from "pinia";
 import api from "@/api/client";
+
+export const UserStore = defineStore("user", {
+    state: () => ({
+        user: {},      // user obj that contains {user_id,username,authenticated}
+        loading: false, // is the action loading? boolean value
+        error: ""       // error message in the event an action fails
+    })
+})
